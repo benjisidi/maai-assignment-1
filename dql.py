f00b6d338c4cc27e00c55fc65e5c062f57c1e06e
Benji Sidi <benjisidi@gmail.com>
2021-09-23 15:48:31 +0100

    Finish q agent and test script

--- dql.py
+++ dql.py
@@ -1,39 +1,81 @@
+
 import torch
+import numpy as np
 
 
-def get_q_network(state_action_dims, action_space):
+def get_q_network(state_action_dims, action_space, hidden_size=100):
     return torch.nn.Sequential(
-        torch.nn.Linear(state_action_dims, int(state_action_dims/2)),
+        torch.nn.Linear(state_action_dims, hidden_size),
         torch.nn.ReLU(),
         torch.nn.Linear(
-            int(state_action_dims/2),
-            int(state_action_dims/4)
+            int(hidden_size),
+            int(hidden_size/2)
         ),
         torch.nn.ReLU(),
-        torch.nn.Linear(int(state_action_dims/4),
+        torch.nn.Linear(int(hidden_size/2),
                         action_space),
     )
 
 
 class DQAgent:
-    def __init__(self, state_size, action_space, discount):
+    def __init__(self, state_size, action_space, discount, epsilon):
         self.state_size = state_size
         self.action_space = action_space
         self.model = get_q_network(state_size, action_space)
         self.optimizer = torch.optim.Adam(params=self.model.parameters())
         self.discount = discount
-        self.prev_output = 0
+        self.prev_output = None
+        self.epsilon = epsilon
 
     def pseudo_loss(self, output, reward, next_state):
-        return 0.5 * (reward + self.discount * (torch.max(self.model.forward(next_state))) - output)
+        return 0.5 * (reward + self.discount * (torch.max(self.model.forward(torch.tensor(next_state)))) - output)
 
-    def step(self, reward, next_state):
+    def act(self, obs, explore):
+        next_values = self.model.forward(obs)
+        if explore:
+            if np.random.rand() < self.epsilon:
+                action = np.random.choice(range(self.action_space))
+                self.prev_output = next_values[action]
+                return action
+        self.prev_output = torch.max(next_values)
+        return torch.argmax(next_values)
+
+    def step(self, reward, next_state, explore=True):
         loss = self.pseudo_loss(self.prev_output, reward, next_state)
         loss.backward()
         self.optimizer.step()
-        next_values = self.model.forward(next_state)
+        return self.act(torch.tensor(next_state), explore=explore)
+
+
+class DQAgentExperience:
+    def __init__(self, state_size, action_space, discount, epsilon):
+        self.state_size = state_size
+        self.action_space = action_space
+        self.model = get_q_network(state_size, action_space)
+        self.optimizer = torch.optim.Adam(params=self.model.parameters())
+        self.discount = discount
+        self.prev_output = None
+        self.epsilon = epsilon
+        self.replay_buffer = []
+
+    def pseudo_loss(self, output, reward, next_state):
+        return 0.5 * (reward + self.discount * (torch.max(self.model.forward(torch.tensor(next_state)))) - output)
+
+    def act(self, obs, explore):
+        next_values = self.model.forward(obs)
+        if explore:
+            if np.random.rand() < self.epsilon:
+                action = np.random.choice(range(self.action_space))
+                self.prev_output = next_values[action]
+                return action
         self.prev_output = torch.max(next_values)
         return torch.argmax(next_values)
+
+    def step(self, reward, next_state, explore=True):
+        loss = self.pseudo_loss(self.prev_output, reward, next_state)
+        loss.backward()
+        self.optimizer.step()
+        return self.act(torch.tensor(next_state), explore=explore)
 
 
 if __name__ == "__main__":
@@ -43,7 +85,6 @@ if __name__ == "__main__":
     state_action_pair = torch.rand(100)
     scores = model.forward(state_action_pair)
     output = loss(scores, torch.tensor([1, 0]).float())
-    print(output)
     output.backward()
     optimiser.step()
     print(model)
--- dql_test.py
+++ dql_test.py
@@ -1,14 +1,34 @@
+import numpy as np
 import gym
 import ma_gym
 from dql import DQAgent
-
+import torch
+from ma_gym.wrappers import Monitor
+from tqdm import trange
+import shutil
 # env.step(agent_actions) -> obs_n, reward_n, done_n, info
 # Full usage here https://github.com/koulanurag/ma-gym/wiki/Usage
 env = gym.make("Switch2-v0")
-agent1 = DQAgent(2, 5, 1)
-agent2 = DQAgent(2, 5, 1)
+shutil.rmtree("recordings")
+env = Monitor(env, directory='recordings',
+              video_callable=lambda episode_id: episode_id % 100 == 0)
+agent1 = DQAgent(2, 5, 1, epsilon=0.1)
+agent2 = DQAgent(2, 5, 1, epsilon=0.1)
 
-done_n = [False, False]
-
-while not all(done_n):
-    pass
+pbar = trange(5000, desc="Prev Reward: ", leave=True)
+for episode in pbar:
+    explore = env.episode_id % 100 == 0
+    total_reward = 0
+    done_n = [False, False]
+    obs_1, obs_2 = env.reset()
+    act_1 = agent1.act(torch.tensor(obs_1), explore=explore)
+    act_2 = agent2.act(torch.tensor(obs_2), explore=explore)
+    obs_n, reward_n, done_n, info = env.step([act_1, act_2])
+    counter = 1
+    while not all(done_n):
+        act_1 = agent1.step(reward_n[0], obs_n[0])
+        act_2 = agent2.step(reward_n[1], obs_n[1])
+        obs_n, reward_n, done_n, info = env.step([act_1, act_2])
+        counter += 1
+        total_reward += np.sum(reward_n)
+    pbar.set_description(f"Prev Reward: {total_reward:.0f}", refresh=True)
