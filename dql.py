e34ac26c259b747eaf4985e3f47eb107bb52fd61
Benji Sidi <benjisidi@gmail.com>
2021-09-23 22:02:42 +0100

    clean up and add double Q learning

--- dql.py
+++ dql.py
@@ -4,7 +4,7 @@ import torch
 import numpy as np
 
 
-def get_q_network(state_action_dims, action_space, hidden_size=32):
+def get_q_network(state_action_dims, action_space, hidden_size=64):
     return torch.nn.Sequential(
         torch.nn.Linear(state_action_dims, hidden_size),
         torch.nn.ReLU(),
@@ -25,69 +25,54 @@ def get_q_network(state_action_dims, action_space, hidden_size=32):
     )
 
 
-class DQAgent:
-    def __init__(self, state_size, action_space, discount, epsilon):
+class ReplayBuffer:
+    def __init__(self, max_size):
+        self.buffer = []
+        self.max_size = max_size
+
+    def save(self, memory):
+        self.buffer.append(memory)
+        if len(self.buffer) > self.max_size:
+            self.buffer.pop(0)
+
+    def sample(self, k):
+        return random.sample(self.buffer, k=k)
+
+    def __len__(self):
+        return len(self.buffer)
+
+
+class DQN:
+    def __init__(self, state_size, action_space, discount, epsilon, batch_size, lr, update_steps, memory_size):
         self.state_size = state_size
         self.action_space = action_space
         self.model = get_q_network(state_size, action_space)
+        self.target_model = get_q_network(state_size, action_space)
         self.optimizer = torch.optim.Adam(
-            params=self.model.parameters(), lr=1e-4)
+            params=self.model.parameters(), lr=lr)
         self.discount = discount
         self.prev_obs = None
         self.prev_action = None
         self.epsilon = epsilon
         self.loss_fn = torch.nn.MSELoss()
-
-    def get_loss(self, prev_state, prev_action, reward, next_state, dead):
-        pred = self.model.forward(prev_state)[prev_action]
-        pred_next = self.model.forward(next_state)
-        updated_pred = reward + (1-dead) * self.discount * torch.max(pred_next)
-        loss = self.loss_fn(pred, updated_pred)
-        return loss
-
-    def act(self, obs, explore):
-        next_values = self.model.forward(obs)
-        self.prev_obs = obs
-        if explore:
-            if np.random.rand() < self.epsilon:
-                action = np.random.choice(range(self.action_space))
-                self.prev_action = action
-                return action
-        self.prev_action = torch.argmax(next_values)
-        return torch.argmax(next_values)
-
-    def step(self, reward, next_state, dead, explore=True):
-        loss = self.get_loss(self.prev_obs, self.prev_action,
-                             reward, next_state, dead)
-        loss.backward()
-        self.optimizer.step()
-        return self.act(next_state, explore=explore)
-
-
-class DQAgentExperience:
-    def __init__(self, state_size, action_space, discount, epsilon, batch_size):
-        self.state_size = state_size
-        self.action_space = action_space
-        self.model = get_q_network(state_size, action_space)
-        self.optimizer = torch.optim.Adam(params=self.model.parameters())
-        self.discount = discount
-        self.prev_obs = None
-        self.prev_action = None
-        self.epsilon = epsilon
-        self.loss_fn = torch.nn.MSELoss()
-        self.replay_buffer = []
+        self.replay_buffer = ReplayBuffer(memory_size)
         self.batch_size = batch_size
+        self.update_steps = update_steps
+        self.update_counter = 0
+
+    def update_target_model(self):
+        self.target_model.load_state_dict(self.model.state_dict())
 
     def get_loss(self, prev_state, prev_action, reward, next_state, dead):
         preds = self.model.forward(prev_state)
         selected_preds = torch.gather(preds, 1, prev_action.unsqueeze(1))
-        pred_next = self.model.forward(next_state)
-        updated_preds = reward + (1-dead) * \
+        pred_next = self.target_model.forward(next_state).detach()
+        target = reward + (1-dead) * \
             self.discount * torch.max(pred_next)
-        loss = self.loss_fn(selected_preds.squeeze(), updated_preds)
+        loss = self.loss_fn(selected_preds.squeeze(), target)
         return loss
 
-    def act(self, obs, episode, eval):
+    def act(self, obs, episode, eval=False):
         if not eval:
             if callable(self.epsilon):
                 explore = np.random.rand() <= self.epsilon(episode)
@@ -105,25 +90,104 @@ class DQAgentExperience:
             return action
         self.prev_action = torch.argmax(next_values)
         self.model.train(True)
-        return torch.argmax(next_values)
+        action = torch.argmax(next_values)
+        return action
 
     def step(self, reward, next_state, dead, episode, eval=False):
         self.save(reward, next_state, dead)
         return self.act(next_state, episode, eval=eval)
 
     def save(self, reward, next_state, dead):
-        self.replay_buffer.append(
+        self.replay_buffer.save(
             [self.prev_obs, self.prev_action, reward, next_state, dead])
 
     def learn(self):
-        replays = random.choices(
-            self.replay_buffer, k=self.batch_size)
+        if self.update_counter % self.update_steps == 0:
+            self.update_target_model()
+        replays = self.replay_buffer.sample(k=self.batch_size)
         obs, acts, rewards, next_states, deads = zip(*replays)
         self.optimizer.zero_grad()
         losses = self.get_loss(torch.stack(obs), torch.stack(acts),
                                torch.tensor(rewards), torch.stack(next_states), torch.tensor(deads, dtype=int))
         losses.backward()
         self.optimizer.step()
+        self.update_counter += 1
+
+
+class DDQN:
+    def __init__(self, state_size, action_space, discount, epsilon, batch_size, lr, update_steps, memory_size):
+        self.state_size = state_size
+        self.action_space = action_space
+        self.model_A = get_q_network(state_size, action_space)
+        self.model_B = get_q_network(state_size, action_space)
+        self.optimizer_A = torch.optim.Adam(
+            params=self.model_A.parameters(), lr=lr)
+        self.optimizer_B = torch.optim.Adam(
+            params=self.model_B.parameters(), lr=lr)
+        self.discount = discount
+        self.prev_obs = None
+        self.prev_action = None
+        self.epsilon = epsilon
+        self.loss_fn = torch.nn.MSELoss()
+        self.replay_buffer = ReplayBuffer(memory_size)
+        self.batch_size = batch_size
+        self.update_steps = update_steps
+        self.update_counter = 0
+
+    def update_target_model(self):
+        self.target_model.load_state_dict(self.model.state_dict())
+
+    def get_loss(self, prev_state, prev_action, reward, next_state, dead, pred_model, target_model):
+        preds = pred_model.forward(prev_state)
+        selected_preds = torch.gather(preds, 1, prev_action.unsqueeze(1))
+        pred_next = target_model.forward(next_state).detach()
+        target = reward + (1-dead) * \
+            self.discount * torch.max(pred_next)
+        loss = self.loss_fn(selected_preds.squeeze(), target)
+        return loss
+
+    def act(self, obs, episode, eval=False):
+        if not eval:
+            if callable(self.epsilon):
+                explore = np.random.rand() <= self.epsilon(episode)
+            else:
+                explore = np.random.rand() <= self.epsilon
+        else:
+            explore = False
+            self.model.train(False)
+        next_values = self.model.forward(obs)
+        self.prev_obs = obs
+        if explore:
+            action = torch.tensor(
+                np.random.choice(range(self.action_space)))
+            self.prev_action = action
+            return action
+        self.prev_action = torch.argmax(next_values)
+        self.model.train(True)
+        action = torch.argmax(next_values)
+        return action
+
+    def step(self, reward, next_state, dead, episode, eval=False):
+        self.save(reward, next_state, dead)
+        return self.act(next_state, episode, eval=eval)
+
+    def save(self, reward, next_state, dead):
+        self.replay_buffer.save(
+            [self.prev_obs, self.prev_action, reward, next_state, dead])
+
+    def learn(self):
+        models = [self.model_A, self.model_B]
+        optimisers = [self.optimizer_A, self.optimizer_B]
+        pred_index = np.random.choice([0, 1])
+        target_index = int(not pred_index)
+        replays = self.replay_buffer.sample(k=self.batch_size)
+        obs, acts, rewards, next_states, deads = zip(*replays)
+        optimisers[pred_index].zero_grad()
+        losses = self.get_loss(torch.stack(obs), torch.stack(acts),
+                               torch.tensor(rewards), torch.stack(next_states), torch.tensor(deads, dtype=int), models[pred_index], models[target_index])
+        losses.backward()
+        optimisers[pred_index].step()
+        self.update_counter += 1
 
 
 if __name__ == "__main__":
--- dql_test.py
+++ dql_test.py
@@ -1,12 +1,13 @@
 import numpy as np
 import gym
 import ma_gym
-from dql import DQAgent, DQAgentExperience
+from dql import DQN, DDQN
 import torch
 from ma_gym.wrappers import Monitor
 from tqdm import trange
 import shutil
 import matplotlib.pyplot as plt
+from time import sleep
 # env.step(agent_actions) -> obs_n, reward_n, done_n, info
 # Full usage here https://github.com/koulanurag/ma-gym/wiki/Usage
 
@@ -15,40 +16,45 @@ def moving_average(data, window):
     return np.convolve(data, np.ones(window), 'valid') / window
 
 
-def test_naive_dqn():
-    env = gym.make("Switch2-v0")
-    shutil.rmtree("recordings")
-    env = Monitor(env, directory='recordings',
-                  video_callable=lambda episode_id: episode_id % 100 == 0)
-    agent1 = DQAgent(2, 5, 1, epsilon=0.1)
-    agent2 = DQAgent(2, 5, 1, epsilon=0.1)
+# def test_naive_dqn():
+#     env = gym.make("Switch2-v0")
+#     shutil.rmtree("recordings")
+#     env = Monitor(env, directory='recordings',
+#                   video_callable=lambda episode_id: episode_id % 100 == 0)
+#     agent1 = DQAgent(2, 5, 1, epsilon=0.1)
+#     agent2 = DQAgent(2, 5, 1, epsilon=0.1)
 
-    pbar = trange(5000, desc="Prev Reward: ", leave=True)
-    for episode in pbar:
-        explore = env.episode_id % 100 == 0
-        total_reward = 0
-        done_n = [False, False]
-        obs_1, obs_2 = env.reset()
-        act_1 = agent1.act(torch.tensor(obs_1), explore=explore)
-        act_2 = agent2.act(torch.tensor(obs_2), explore=explore)
-        obs_n, reward_n, done_n, info = env.step([act_1, act_2])
-        counter = 1
-        while not all(done_n):
-            act_1 = agent1.step(reward_n[0], torch.tensor(obs_n[0]), done_n[0])
-            act_2 = agent2.step(reward_n[1], torch.tensor(obs_n[1]), done_n[1])
-            obs_n, reward_n, done_n, info = env.step([act_1, act_2])
-            counter += 1
-            total_reward += np.sum(reward_n)
-        pbar.set_description(f"Prev Reward: {total_reward:.0f}", refresh=True)
+#     pbar = trange(5000, desc="Prev Reward: ", leave=True)
+#     for episode in pbar:
+#         explore = env.episode_id % 100 == 0
+#         total_reward = 0
+#         done_n = [False, False]
+#         obs_1, obs_2 = env.reset()
+#         act_1 = agent1.act(torch.tensor(obs_1), explore=explore)
+#         act_2 = agent2.act(torch.tensor(obs_2), explore=explore)
+#         obs_n, reward_n, done_n, info = env.step([act_1, act_2])
+#         counter = 1
+#         while not all(done_n):
+#             act_1 = agent1.step(reward_n[0], torch.tensor(obs_n[0]), done_n[0])
+#             act_2 = agent2.step(reward_n[1], torch.tensor(obs_n[1]), done_n[1])
+#             obs_n, reward_n, done_n, info = env.step([act_1, act_2])
+#             counter += 1
+#             total_reward += np.sum(reward_n)
+#         pbar.set_description(f"Prev Reward: {total_reward:.0f}", refresh=True)
 
 
 def test_replay_dqn():
-    batch_size = 32
-    n_episodes = 12000
+    batch_size = 64
+    n_episodes = 5000
     epsilon_start = 0.2
     epsilon_end = 0.05
-    decay_end = 12000
-
+    decay_end = 20000
+    lr = 1e-4
+    discount_factor = 0.99
+    update_steps = 50
+    memory_size = 3000
+    update_frequency = 2
+    replay_start_size = 100
     env = gym.make("Switch2-v0")
     eval_env = gym.make("Switch2-v0")
     shutil.rmtree("recordings", ignore_errors=True)
@@ -58,8 +64,10 @@ def test_replay_dqn():
     eval_env = Monitor(eval_env, directory="eval_recordings")
     def epsilon(x): return (epsilon_start-epsilon_end) * \
         ((decay_end - x)/decay_end) + epsilon_end if x < decay_end else 0.01
-    agent1 = DQAgentExperience(2, 5, 1, epsilon=0.17, batch_size=batch_size)
-    agent2 = DQAgentExperience(2, 5, 1, epsilon=0.17, batch_size=batch_size)
+    agent1 = DQN(
+        2, 5, discount=discount_factor, epsilon=epsilon, batch_size=batch_size, lr=lr, update_steps=update_steps, memory_size=memory_size)
+    agent2 = DQN(
+        2, 5, discount=discount_factor, epsilon=epsilon, batch_size=batch_size, lr=lr, update_steps=update_steps, memory_size=memory_size)
 
     pbar = trange(n_episodes, desc="Prev Reward: ", leave=True)
     episode_rewards = []
@@ -69,55 +77,57 @@ def test_replay_dqn():
         total_reward = 0
         done_n = [False, False]
         obs_1, obs_2 = env.reset()
-        act_1 = agent1.act(torch.tensor(obs_1), episode=episode, eval=eval_)
-        act_2 = agent2.act(torch.tensor(obs_2), episode=episode, eval=eval_)
+        act_1 = agent1.act(torch.tensor(obs_1), episode=episode)
+        act_2 = agent2.act(torch.tensor(obs_2), episode=episode)
         obs_n, reward_n, done_n, info = env.step([act_1, act_2])
         counter = 1
         while not all(done_n):
             act_1 = agent1.step(reward_n[0], torch.tensor(
-                obs_n[0]), done_n[0], episode=episode, eval=eval_)
+                obs_n[0]), done_n[0], episode=episode)
             act_2 = agent2.step(reward_n[1], torch.tensor(
-                obs_n[1]), done_n[1], episode=episode, eval=eval_)
+                obs_n[1]), done_n[1], episode=episode)
             obs_n, reward_n, done_n, info = env.step([act_1, act_2])
             counter += 1
             total_reward += np.sum(reward_n)
+
+            if len(agent1.replay_buffer) > replay_start_size and counter % update_frequency == 0:
+                agent1.learn()
+                agent2.learn()
+
         agent1.save(reward_n[0], torch.tensor(obs_n[0]),  done_n[0])
         agent2.save(reward_n[1], torch.tensor(obs_n[1]),  done_n[1])
         episode_rewards.append(total_reward)
         pbar.set_description(
             f"Avg Reward: {np.mean(episode_rewards[-20:]):2.0f}", refresh=True)
 
-        if len(agent1.replay_buffer) > batch_size:
-            agent1.learn()
-            agent2.learn()
-
-        if episode % 100 == 0:
-            eval_reward = 0
-            eval_done_n = [False, False]
-            eval_obs_1, eval_obs_2 = eval_env.reset()
-            eval_act_1 = agent1.act(torch.tensor(
-                eval_obs_1), episode=episode, eval=True)
-            eval_act_2 = agent2.act(torch.tensor(
-                eval_obs_2), episode=episode, eval=True)
-            eval_obs_n, eval_reward_n, eval_done_n, info = eval_env.step(
-                [eval_act_1, eval_act_2])
-            counter = 1
-            while not all(eval_done_n):
-                eval_act_1 = agent1.step(eval_reward_n[0], torch.tensor(
-                    eval_obs_n[0]), eval_done_n[0], episode=episode, eval=True)
-                eval_act_2 = agent2.step(eval_reward_n[1], torch.tensor(
-                    eval_obs_n[1]), eval_done_n[1], episode=episode, eval=True)
-                eval_obs_n, eval_reward_n, eval_done_n, info = eval_env.step([
-                    eval_act_1, eval_act_2])
-                counter += 1
-                eval_reward += np.sum(eval_reward_n)
-            eval_rewards.append(eval_reward)
+        # if eval_ and episode % 100 == 0:
+        #     eval_reward = 0
+        #     eval_done_n = [False, False]
+        #     eval_obs_1, eval_obs_2 = eval_env.reset()
+        #     eval_act_1 = agent1.act(torch.tensor(
+        #         eval_obs_1), episode=episode, eval=True)
+        #     eval_act_2 = agent2.act(torch.tensor(
+        #         eval_obs_2), episode=episode, eval=True)
+        #     eval_obs_n, eval_reward_n, eval_done_n, info = eval_env.step(
+        #         [eval_act_1, eval_act_2])
+        #     counter = 1
+        #     while not all(eval_done_n):
+        #         eval_act_1 = agent1.step(eval_reward_n[0], torch.tensor(
+        #             eval_obs_n[0]), eval_done_n[0], episode=episode, eval=True)
+        #         eval_act_2 = agent2.step(eval_reward_n[1], torch.tensor(
+        #             eval_obs_n[1]), eval_done_n[1], episode=episode, eval=True)
+        #         eval_obs_n, eval_reward_n, eval_done_n, info = eval_env.step([
+        #             eval_act_1, eval_act_2])
+        #         counter += 1
+        #         eval_reward += np.sum(eval_reward_n)
+        #     eval_rewards.append(eval_reward)
 
     plot_results(episode_rewards, epsilon)
-    plt.figure()
-    plt.plot(np.arange(len(eval_rewards))*100,
-             eval_rewards, label="Eval. Reward")
-    plt.legend()
+    if eval_:
+        plt.figure()
+        plt.plot(np.arange(len(eval_rewards))*100,
+                 eval_rewards, label="Eval. Reward")
+        plt.legend()
     plt.show()
 
 
