648ae064772be61b6f9d346abdeee3bf8fa0cd92
Benji Sidi <benjisidi@gmail.com>
2021-09-24 13:26:27 +0100

    Better replay buffer and prioritization!

--- dql.py
+++ dql.py
@@ -28,10 +28,13 @@ def get_q_network(state_action_dims, action_space, hidden_size=64):
 class ReplayBuffer:
     def __init__(self, max_size):
         self.buffer = []
+        self.weights = np.array([])
         self.max_size = max_size
 
-    def save(self, memory):
+    def save(self, memory, weight):
         self.buffer.append(memory)
+        self.weights.append()
+        self.weights.append()
         if len(self.buffer) > self.max_size:
             self.buffer.pop(0)
 
@@ -40,6 +43,37 @@ class ReplayBuffer:
 
     def __len__(self):
         return len(self.buffer)
+
+
+class NumpyReplayBuffer:
+    def __init__(self, max_len, width):
+        self.buffer = np.empty((max_len, width))
+        self.weights = np.zeros(max_len)
+        self.insertion_index = 0
+        self.max_len = max_len
+        self.full = False
+
+    def save(self, row, weight):
+        self.buffer[self.insertion_index] = row
+        self.weights[self.insertion_index] = weight
+
+        if self.insertion_index == self.max_len - 1:
+            self.insertion_index = 0
+            self.full = True
+        else:
+            self.insertion_index += 1
+
+    def sample(self, k):
+        sample_idx = np.random.choice(
+            np.arange(self.max_len, dtype=int), size=k, replace=False, p=self.weights/np.sum(self.weights))
+        examples = self.buffer[sample_idx]
+        return examples, sample_idx
+
+    def update_weights(self, new_weights, idx):
+        self.weights[idx] = new_weights
+
+    def __len__(self):
+        return self.insertion_index if not self.full else self.max_len
 
 
 class DQN:
@@ -55,7 +89,8 @@ class DQN:
         self.prev_action = None
         self.epsilon = epsilon
         self.loss_fn = torch.nn.MSELoss()
-        self.replay_buffer = ReplayBuffer(memory_size)
+        # S+S' = state_size *2, action + reward + done = 3
+        self.replay_buffer = NumpyReplayBuffer(memory_size, state_size * 2 + 3)
         self.batch_size = batch_size
         self.update_steps = update_steps
         self.update_counter = 0
@@ -67,12 +102,14 @@ class DQN:
 
     def get_loss(self, prev_state, prev_action, reward, next_state, dead):
         preds = self.model.forward(prev_state)
-        selected_preds = torch.gather(preds, 1, prev_action.unsqueeze(1))
+        selected_preds = torch.gather(
+            preds, 1, prev_action.unsqueeze(1).to(torch.int64))
         pred_next = self.target_model.forward(next_state).detach()
         target = reward + (1-dead) * \
             self.discount * torch.max(pred_next)
+        delta = selected_preds.squeeze() - target
         loss = self.loss_fn(selected_preds.squeeze(), target)
-        return loss
+        return loss, np.absolute(delta.detach().numpy())
 
     def act(self, obs, episode):
         if callable(self.epsilon):
@@ -102,17 +139,24 @@ class DQN:
         return self.act(next_state, episode)
 
     def save(self, reward, next_state, dead):
+        # New experiences are saved with a very large weight to ensure each is seen at least once
         self.replay_buffer.save(
-            [self.prev_obs, self.prev_action, reward, next_state, dead])
+            [*self.prev_obs.numpy(), self.prev_action.item(), reward, *next_state.numpy(), int(dead)], 99999)
 
     def learn(self):
         if self.update_counter % self.update_steps == 0:
             self.update_target_model()
-        replays = self.replay_buffer.sample(k=self.batch_size)
-        obs, acts, rewards, next_states, deads = zip(*replays)
+        replays, idx = self.replay_buffer.sample(k=self.batch_size)
+        replays = torch.tensor(replays).float()
+        obs = replays[:, :self.state_size]
+        acts = replays[:, self.state_size]
+        rewards = replays[:, self.state_size + 1]
+        next_states = replays[:, self.state_size + 2: self.state_size*2 + 2]
+        deads = replays[:, -1].int()
         self.optimizer.zero_grad()
-        losses = self.get_loss(torch.stack(obs), torch.stack(acts),
-                               torch.tensor(rewards), torch.stack(next_states), torch.tensor(deads, dtype=int))
+        losses, new_weights = self.get_loss(
+            obs, acts, rewards, next_states, deads)
+        self.replay_buffer.update_weights(new_weights, idx)
         losses.backward()
         self.optimizer.step()
         self.update_counter += 1
@@ -185,14 +229,15 @@ class DDQN:
 
     def save(self, reward, next_state, dead):
         self.replay_buffer.save(
-            [self.prev_obs, self.prev_action, reward, next_state, dead])
+            [*self.prev_obs, self.prev_action, reward, *next_state, dead])
 
     def learn(self):
         models = [self.model_A, self.model_B]
         optimisers = [self.optimizer_A, self.optimizer_B]
         pred_index = np.random.choice([0, 1])
         target_index = int(not pred_index)
-        replays = self.replay_buffer.sample(k=self.batch_size)
+        replays, idx = self.replay_buffer.sample(k=self.batch_size)
+
         obs, acts, rewards, next_states, deads = zip(*replays)
         optimisers[pred_index].zero_grad()
         losses = self.get_loss(torch.stack(obs), torch.stack(acts),
--- dql_test.py
+++ dql_test.py
@@ -16,37 +16,10 @@ def moving_average(data, window):
     return np.convolve(data, np.ones(window), 'valid') / window
 
 
-# def test_naive_dqn():
-#     env = gym.make("Switch2-v0")
-#     shutil.rmtree("recordings")
-#     env = Monitor(env, directory='recordings',
-#                   video_callable=lambda episode_id: episode_id % 100 == 0)
-#     agent1 = DQAgent(2, 5, 1, epsilon=0.1)
-#     agent2 = DQAgent(2, 5, 1, epsilon=0.1)
-
-#     pbar = trange(5000, desc="Prev Reward: ", leave=True)
-#     for episode in pbar:
-#         explore = env.episode_id % 100 == 0
-#         total_reward = 0
-#         done_n = [False, False]
-#         obs_1, obs_2 = env.reset()
-#         act_1 = agent1.act(torch.tensor(obs_1), explore=explore)
-#         act_2 = agent2.act(torch.tensor(obs_2), explore=explore)
-#         obs_n, reward_n, done_n, info = env.step([act_1, act_2])
-#         counter = 1
-#         while not all(done_n):
-#             act_1 = agent1.step(reward_n[0], torch.tensor(obs_n[0]), done_n[0])
-#             act_2 = agent2.step(reward_n[1], torch.tensor(obs_n[1]), done_n[1])
-#             obs_n, reward_n, done_n, info = env.step([act_1, act_2])
-#             counter += 1
-#             total_reward += np.sum(reward_n)
-#         pbar.set_description(f"Prev Reward: {total_reward:.0f}", refresh=True)
-
-
 def test_replay_dqn(lr):
     batch_size = 32
-    n_episodes = 40000
-    epsilon_start = 1
+    n_episodes = 2000
+    epsilon_start = 0.2
     epsilon_end = 0.2
     decay_end = 30000
     # lr = 1e-3
@@ -83,7 +56,7 @@ def test_replay_dqn(lr):
         obs_n, reward_n, done_n, info = env.step(acts)
         counter = 1
         while not all(done_n):
-            # env.render()
+            env.render()
             acts = [agents[i].step(reward_n[i], torch.tensor(
                 obs_n[i]), done_n[i], episode=episode) for i in range(env.n_agents)]
             obs_n, reward_n, done_n, info = env.step(acts)
